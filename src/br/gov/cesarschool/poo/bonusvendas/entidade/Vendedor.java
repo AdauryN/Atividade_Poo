07c9aba945aad61bd668dac34e427c77b8245d55
Adaury Neto <91835392+Adaurekt@users.noreply.github.com>
2023-10-18 06:42:35 -0300

    Mudança de Classe

--- src/br/gov/cesarschool/poo/bonusvendas/entidade/Vendedor.java
+++ src/br/gov/cesarschool/poo/bonusvendas/entidade/Vendedor.java
@@ -64,45 +64,6 @@ public class Vendedor {
 		this.renda = renda;
 	}
 
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
 	 public int calcularIdade() {
 	        LocalDate dataAtual = LocalDate.now();
 	        Period periodo = Period.between(dataNascimento, dataAtual);

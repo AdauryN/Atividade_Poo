dbd0b70a05f59772790e81baf90df0ba8f3eb40e
theomilll <tam4@cesar.school>
2023-10-29 20:07:18 -0300

    .

--- src/br/gov/cesarschool/poo/bonusvendas/tela/TelaManutencaoVendedor.java
+++ src/br/gov/cesarschool/poo/bonusvendas/tela/TelaManutencaoVendedor.java
@@ -1,168 +1,180 @@
 package br.gov.cesarschool.poo.bonusvendas.tela;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
-
-import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Sexo;
+import java.awt.event.ActionListener;
 import br.gov.cesarschool.poo.bonusvendas.negocio.VendedorMediator;
-
+import br.gov.cesarschool.poo.bonusvendas.negocio.ResultadoInclusaoVendedor;
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Endereco;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Sexo;
 import java.time.LocalDate;
-
-
+import java.time.format.DateTimeFormatter;
 
 public class TelaManutencaoVendedor extends JFrame {
+  
+    // 1. Create a mediator attribute of type VendedorMediator
+    // Initialize it with the Singleton instance.
+    private VendedorMediator mediator = VendedorMediator.getInstance();
 
-	private JPanel contentPane;
-	private JTextField txtCPF;
-	private JTextField txtNomeCompleto;
-	private JTextField txtDataNascimento;
-	private JTextField txtRenda;
-	private JTextField txtLogradouro;
-	private JTextField txtNumero;
-	private JTextField txtComplemento;
-	private JTextField txtCEP;
-	private JTextField txtCidade;
-	private JComboBox<String> cbEstado;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private VendedorMediator mediator = VendedorMediator.getInstance();
+    // Fields on the screen
+    private JTextField txtCPF;
+    private JTextField txtFullName;
+    private JRadioButton rbMale, rbFemale;
+    private JTextField txtDateOfBirth;
+    private JTextField txtIncome;
+    // ... (other fields to be added)
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TelaManutencaoVendedor frame = new TelaManutencaoVendedor();
+            frame.setVisible(true);
+        });
+    }
+    
+    public TelaManutencaoVendedor() {
+        setTitle("Tela Manutenção Vendedor");
+        setSize(400, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaManutencaoVendedor frame = new TelaManutencaoVendedor();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+        // Initialize components and add them to JFrame
+        initializeComponents();  // This line was missing
+    }
 
-	public TelaManutencaoVendedor() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 400);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+    private void initializeComponents() {
+        txtCPF = new JTextField(15);
+        txtFullName = new JTextField(30);
+        rbMale = new JRadioButton("Male");
+        rbFemale = new JRadioButton("Female");
+        txtDateOfBirth = new JTextField(10);
+        txtIncome = new JTextField(10);
+        ButtonGroup genderGroup = new ButtonGroup();
+        genderGroup.add(rbMale);
+        genderGroup.add(rbFemale);
+        
+        add(new JLabel("CPF:"));
+        add(txtCPF);
+        add(new JLabel("Full Name:"));
+        add(txtFullName);
+        add(new JLabel("Gender:"));
+        add(rbMale);
+        add(rbFemale);
+        add(new JLabel("Date of Birth:"));
+        add(txtDateOfBirth);
+        add(new JLabel("Income:"));
+        add(txtIncome);
+        
+        // ... (Add other components)
+        
+        JButton btnSave = new JButton("Save");
+        btnSave.addActionListener(new SaveButtonHandler());
+        add(btnSave);
+    }
 
-		JLabel lblCPF = new JLabel("CPF:");
-		lblCPF.setBounds(20, 20, 80, 20);
-		contentPane.add(lblCPF);
+    // Event handler for Save button
+    private class SaveButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        	JButton btnSave = new JButton("Save");
+            btnSave.addActionListener(new ActionListener() {
+            	public void actionPerformed(ActionEvent e) {
+            		if (!validateInput()) {
+                        return;
+                    }
+            	    // Collect data from Vendedor fields
+            	    String cpf = txtCPF.getText();
+            	    String nomeCompleto = txtFullName.getText();
+            	    Sexo sexo = rbMale.isSelected() ? Sexo.MASCULINO : Sexo.FEMININO;
+            	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            	    String dateText = txtDateOfBirth.getText();
+            	    LocalDate dataNascimento = LocalDate.parse(dateText, formatter);
+            	    String incomeText = txtIncome.getText();
+            	    double renda = Double.parseDouble(incomeText);
+            	    
+            	    // Collect data from Endereco fields
+            	    JTextComponent txtLogradouro = null;
+    				String logradouro = txtLogradouro.getText();
+            	    JTextComponent txtNumero = null;
+    				int numero = Integer.parseInt(txtNumero.getText());
+            	    JTextComponent txtComplemento = null;
+    				String complemento = txtComplemento.getText();
+            	    JTextComponent txtCEP = null;
+    				String cep = txtCEP.getText();
+            	    JTextComponent txtCidade = null;
+    				String cidade = txtCidade.getText();
+    				JComboBox<String> cbEstado = null;
+    				String estado = (String) cbEstado.getSelectedItem(); // Assuming it's a JComboBox
+            	    String pais = "Brasil"; // Fixed value as mentioned in the specification
 
-		txtCPF = new JTextField();
-		txtCPF.setBounds(120, 20, 150, 20);
-		contentPane.add(txtCPF);
+            	    // Create an Endereco object
+            	    Endereco endereco = new Endereco(logradouro, numero, complemento, cep, cidade, estado, pais);
 
-		JLabel lblNomeCompleto = new JLabel("Nome Completo:");
-		lblNomeCompleto.setBounds(20, 50, 100, 20);
-		contentPane.add(lblNomeCompleto);
+            	    // Create a Vendedor object
+            	    Vendedor newVendedor = new Vendedor(cpf, nomeCompleto, sexo, dataNascimento, renda, endereco);
 
-		txtNomeCompleto = new JTextField();
-		txtNomeCompleto.setBounds(120, 50, 300, 20);
-		contentPane.add(txtNomeCompleto);
+            	    // Call the mediator to include or alter the Vendedor
+            	    VendedorMediator mediator = VendedorMediator.getInstance();
+            	    String result;
+            	    Vendedor existingVendedor = mediator.buscar(newVendedor.getCPF());
+            	    if (existingVendedor == null) {
+            	        ResultadoInclusaoVendedor resultadoInclusao = mediator.incluir(newVendedor);
+            	        result = resultadoInclusao.getMensagemErroValidacao();
+            	    } else {
+            	        result = mediator.alterar(newVendedor);
+            	    }
 
-		JRadioButton rbMasculino = new JRadioButton("Masculino");
-		rbMasculino.setBounds(120, 80, 100, 20);
-		buttonGroup.add(rbMasculino);
-		contentPane.add(rbMasculino);
+            	    // Check the result
+            	    if (result == null) {
+            	        JOptionPane.showMessageDialog(null, "Operation Successful!");
+            	    } else {
+            	        JOptionPane.showMessageDialog(null, "Error: " + result);
+            	    }
+            	}
 
-		JRadioButton rbFeminino = new JRadioButton("Feminino");
-		rbFeminino.setBounds(220, 80, 100, 20);
-		buttonGroup.add(rbFeminino);
-		contentPane.add(rbFeminino);
+            });
+            add(btnSave);        }
+    }
+    
+    // Helper methods for validation, data conversion, etc.
+    private boolean validateInput() {
+        // Check if CPF is empty
+        if (txtCPF.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "CPF cannot be empty!");
+            return false;
+        }
+        
+        // Check if full name is empty
+        if (txtFullName.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Full name cannot be empty!");
+            return false;
+        }
+        
+        // Check if gender is selected
+        if (!rbMale.isSelected() && !rbFemale.isSelected()) {
+            JOptionPane.showMessageDialog(null, "Please select a gender!");
+            return false;
+        }
+        
+        // Check if date of birth is empty
+        if (txtDateOfBirth.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Date of Birth cannot be empty!");
+            return false;
+        }
+        
+        // Check if income is empty or invalid
+        try {
+            Double.parseDouble(txtIncome.getText());
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid income value!");
+            return false;
+        }
+        
+        // Add additional validations as needed
+        
+        return true;
+    }
 
-		JLabel lblLogradouro = new JLabel("Logradouro:");
-		lblLogradouro.setBounds(20, 110, 100, 20);
-		contentPane.add(lblLogradouro);
-
-		txtLogradouro = new JTextField();
-		txtLogradouro.setBounds(120, 110, 300, 20);
-		contentPane.add(txtLogradouro);
-
-		JLabel lblNumero = new JLabel("Número:");
-		lblNumero.setBounds(20, 140, 100, 20);
-		contentPane.add(lblNumero);
-
-		txtNumero = new JTextField();
-		txtNumero.setBounds(120, 140, 300, 20);
-		contentPane.add(txtNumero);
-
-		JLabel lblComplemento = new JLabel("Complemento:");
-		lblComplemento.setBounds(20, 170, 100, 20);
-		contentPane.add(lblComplemento);
-
-		txtComplemento = new JTextField();
-		txtComplemento.setBounds(120, 170, 300, 20);
-		contentPane.add(txtComplemento);
-
-		JLabel lblCEP = new JLabel("CEP:");
-		lblCEP.setBounds(20, 200, 100, 20);
-		contentPane.add(lblCEP);
-
-		txtCEP = new JTextField();
-		txtCEP.setBounds(120, 200, 300, 20);
-		contentPane.add(txtCEP);
-
-		JLabel lblCidade = new JLabel("Cidade:");
-		lblCidade.setBounds(20, 230, 100, 20);
-		contentPane.add(lblCidade);
-
-		txtCidade = new JTextField();
-		txtCidade.setBounds(120, 230, 300, 20);
-		contentPane.add(txtCidade);
-
-		JLabel lblEstado = new JLabel("Estado:");
-		lblEstado.setBounds(20, 260, 100, 20);
-		contentPane.add(lblEstado);
-
-		cbEstado = new JComboBox<>(new String[] { "Selecione", "SP", "RJ", "MG", "PR" });
-		cbEstado.setBounds(120, 260, 300, 20);
-		contentPane.add(cbEstado);
-
-		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.setBounds(120, 290, 100, 30);
-		contentPane.add(btnCadastrar);
-
-		JButton btnAlterar = new JButton("Alterar");
-		btnAlterar.setBounds(230, 290, 100, 30);
-		contentPane.add(btnAlterar);
-
-		btnCadastrar.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		        String cpf = txtCPF.getText();
-		        String nomeCompleto = txtNomeCompleto.getText();
-		        Sexo sexo = rbMasculino.isSelected() ? Sexo.MASCULINO : Sexo.FEMININO;
-		        LocalDate dataNascimento = LocalDate.parse(txtDataNascimento.getText());
-		        double renda = Double.parseDouble(txtRenda.getText());
-		        String logradouro = txtLogradouro.getText();
-		        int numero = Integer.parseInt(txtNumero.getText());
-		        String complemento = txtComplemento.getText();
-		        String cep = txtCEP.getText();
-		        String cidade = txtCidade.getText();
-		        String estado = cbEstado.getSelectedItem().toString();
-		        String pais = "Brasil";
-		        
-
-		        mediator.cadastrarVendedor(cpf, nomeCompleto, sexo, dataNascimento, renda, logradouro, numero, complemento, cep, cidade, estado, pais);
-		    }
-		});
-
-
-		btnAlterar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-	}
+    // ... (other functionalities)
 }
